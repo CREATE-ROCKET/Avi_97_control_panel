8023fb48d04c8d135902847c1305e86f5c054d32
Kuh456 <kudog1106@gmail.com>
2025-09-28 18:19:49 +0900

    refactor : CAN周辺をESP lib準拠に変更し不要な変数を消去

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include "CANCREATE.h"
 /*GPIOの値の設定*/
-#define serial1RX 21
+#define serial1RX 21 /*シリアル変換モジュール*/
 #define serial1TX 18
 #define FILL 16
 #define VALVESET 4
@@ -11,14 +11,14 @@
 #define MCU_LUMP 15
 #define CAN_TX 32
 #define CAN_RX 33
+#define NICHROME_SIGNAL 0x10a
 
-constexpr int readpins[5] = {GPIO_NUM_16, GPIO_NUM_4, GPIO_NUM_34, GPIO_NUM_35, GPIO_NUM_17};
-bool PLC_DEAD = true;
+constexpr short readpins[5] = {GPIO_NUM_16, GPIO_NUM_4, GPIO_NUM_34, GPIO_NUM_35, GPIO_NUM_17};
 unsigned long lastTime = 0;
-int led_blink_count = 0;
+short led_blink_count = 0;
 /*以下はデバウンス用*/
-int iffirepushingwithdebouce = 0;
-int ifFDpushingwithdebouce = 0;
+char iffirepushingwithdebouce = 0;
+char ifFDpushingwithdebouce = 0;
 unsigned long firedebouncetime = 0;
 unsigned long FDdebouncetime = 0;
 bool firecheck = false;
@@ -27,13 +27,12 @@ CAN_CREATE CAN(true);
 void setup()
 {
   pinMode(MCU_LUMP, OUTPUT);
-  // Initialize configuration structures using macro initializers
   Serial.begin(115200);
   Serial1.begin(115200, SERIAL_8N1, serial1RX, serial1TX);
   // tx,rx  32,33:コンパネ　　13,27:中継
   Serial.println("CAN Sender");
   // 100 kbpsでCANを動作させる
-  if (CAN.begin(100E3, CAN_RX, CAN_TX, 10))
+  if (CAN.begin(100E3, CAN_RX, CAN_TX))
   {
     Serial.println("Starting CAN failed!");
     while (1)
@@ -54,7 +53,6 @@ void checkPLCisDEAD()
   }
   else
   {
-    PLC_DEAD = true;
     if (led_blink_count % 2 == 0)
     {
       digitalWrite(MCU_LUMP, LOW);
@@ -65,30 +63,21 @@ void checkPLCisDEAD()
     }
     ++led_blink_count;
   }
+  if (led_blink_count == 1000)
+  {
+    led_blink_count = 0;
+  }
 }
 
 void send()
-{ // send the switch data to the CAN bus in id 0x101
-  uint8_t message[4];
-  for (int i = 0; i < 4; i++)
-  {
-    message[i] = 0;
-  }
-  // for (int j = 0; j < 5; j++)
-  // {
-  //   message.data[0]|=(digitalRead(readpins[j])&1)<<j;
-  // }
+{ // send the switch data to the CAN bus in id 0x101(to plc)
   uint8_t data = 0;
   data |= (digitalRead(DUMP) & 1) << 0;
   data |= (digitalRead(FILL) & 1) << 1;
   data |= ((iffirepushingwithdebouce & (!digitalRead(FD))) & 1) << 2;
   data |= (ifFDpushingwithdebouce) << 3;
   data |= (digitalRead(VALVESET) & 1) << 4;
-  message[0] = data;
-
-  Serial.println(message[0], BIN);
-  // Queue message for transmission
-  if (CAN.sendData(0x101, message, 4))
+  if (CAN.sendData(0x101, &data, 1))
   {
     Serial.println("failed to send CAN data");
   }
@@ -139,61 +128,33 @@ void loop()
   checkPLCisDEAD();
   send();
   updatepins();
-  if (Serial.available())
-  {
-    uint8_t data[8] = {1, 2, 3, 4, 5, 6, 7, 8};
-    char c = Serial.read();
-    switch (c)
-    {
-    case 1: /*テスト*/
-      if (CAN.sendData(0x7ff, data, 8))
-      {
-        Serial.println("failed to send CAN data");
-      }
-      break;
-    case 2: /* サーボ角度リクエストテスト*/
-      if (CAN.sendData(0x201, data, 8))
-      {
-        Serial.println("failed to send CAN data");
-      }
-      break;
-    case 3: /* サーボ角度リクエストテスト*/
-      if (CAN.sendData(0x301, data, 8))
-      {
-        Serial.println("failed to send CAN data");
-      }
-      break;
-    default:
-      break;
-    }
-  }
   if (CAN.available())
   {
     can_return_t message;
     while (!CAN.readWithDetail(&message))
     {
-      if (message.id == 0x402)
+      switch (message.id)
       {
-        Serial1.print("FD angle is");
-        Serial1.println(message.data[0] - 128, DEC);
-        send();
-      }
-      if (message.id == 0x401)
-      {
+      case 0x401: /*from main_valve*/
         Serial1.print("innner angle is");
         Serial1.println(message.data[0] - 128, DEC);
         send();
-      }
-      if (message.id == 0x403)
-      {
-        // Serial1.print("plc is alive!");
-        PLC_DEAD = false;
+        break;
+      case 0x402: /*from fd_valve*/
+        Serial1.print("FD angle is");
+        Serial1.println(message.data[0] - 128, DEC);
+        send();
+        break;
+      case 0x403: /*from plc*/
         lastTime = millis();
         //   for (int i = 0; i < message.size; i++)
         //   {
         //     Serial1.print(message.data[i], BIN);
         //     Serial1.print(" ");
         //   }
+        break;
+      default:
+        break;
       }
       delay(1);
     }
